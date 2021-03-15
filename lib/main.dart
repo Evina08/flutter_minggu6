853113c5d9a26b7cbe7e03faf2152bd0b23fce70
Evina <64392104+Evina08@users.noreply.github.com>
2021-03-15 15:49:58 +0700

    Tampilan card

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,15 @@ import 'package:flutter_minggu5/pages/home_page.dart';
 import 'package:flutter_minggu5/pages/item_page.dart';
 
 void main() {
-  runApp(MaterialApp(initialRoute: '/', routes: {
-    '/': (context) => HomePage(),
-    '/item': (context) => ItemPage(),
-  }));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(initialRoute: '/', routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => ItemPage(),
+    });
+  }
 }
