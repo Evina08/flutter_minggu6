2a0a9c991f57df56e80c85a45030dc79d34c036e
Evina <64392104+Evina08@users.noreply.github.com>
2021-03-15 14:32:23 +0700

    Membuat ListView dari object Item

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_minggu5/models/item.dart';
 
 class HomePage extends StatelessWidget {
+  final List<Item> items = [
+    Item(name: 'Sugar', price: 5000),
+    Item(name: 'Salt', price: 2000)
+  ];
   @override
   Widget build(BuildContext context) {
     throw UnimplementedError();
